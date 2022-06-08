a69cf880518f783b62690936a63255b1ca741e2c
Ben Letton <benletton@gmail.com>
2022-06-08 15:45:21 +0100

    progress

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,15 @@ module.exports = {
     title: `Toldspaces`,
     siteUrl: `https://www.toldspaces.com`
   },
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+      },
+    },
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,31 @@
 import * as React from "react"
 import MainLayout from "../layout";
-import LocationComponent from "../components/Location/Location";
-import locations from "../data/locations";
-import {Link} from "gatsby";
+import {graphql, useStaticQuery} from "gatsby";
 
 
 // markup
 const IndexPage = () => {
+
+	const data = useStaticQuery(graphql`
+	query LocationsQuery {
+		allLocationsJson{
+			edges{
+				node{
+					path
+					text
+				}
+			}
+		}
+	}`)
+
+	console.log({data})
+
+
 	return (
 			<MainLayout>
 				{
-					locations.map(location => (
-							// T1, T2, T3
-							<p><Link to={location.path}>{location.text}</Link></p>
-							// <LocationComponent key={location.latLng} />
-							// <li key={location.latLng}>
-							// 	<span>
-							// 		<a href={`${location.latLng}`}>
-							// 		</a>
-							// 		<p>{location.description}</p>
-							// 	</span>
-							// </li>
+					data.allLocationsJson.edges.map( location => (
+							<p key={location.path}>{location.text}</p>
 					))
 				}
 			</MainLayout>
